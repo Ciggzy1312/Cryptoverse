ad9c9d10f06d43d92fd2d4b371491df6fa6fa19d
Deepayan <deepayanmukherjee1312@gmail.com>
2021-09-22 20:23:54 +0530

    setup main section

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 import { Layout, Space, Typography } from 'antd'
 
-import {Navbar} from './components'
+import { Navbar, HomePage, Cryptocurrencies, Exchanges, CryptoDetails, News } from './components'
+import './App.css'
 
 const App = () => {
     return (
         <Router>
-            <div>
+            <div className='app'>
                 <div className='navbar'>
                     <Navbar />
+                </div>
+
+                <div className='main'>
+                    <Layout>
+                        <Switch>
+                            <Route exact path='/'><HomePage /></Route>
+
+                            <Route exact path='/cryptocurrencies'><Cryptocurrencies /></Route>
+
+                            <Route exact path='/exchanges'><Exchanges /></Route>
+
+                            <Route exact path='/crypto/:coinID'><CryptoDetails /></Route>
+
+                            <Route exact path='/news'><News /></Route>
+                        </Switch>
+                    </Layout>
                 </div>
             </div>
         </Router>
