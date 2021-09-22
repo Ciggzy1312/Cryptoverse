9d63e330c49a8c580f6439d131ad7c65cb3efc3b
Deepayan <deepayanmukherjee1312@gmail.com>
2021-09-22 20:37:30 +0530

    Added footer

--- src/App.js
+++ src/App.js
@@ -27,6 +27,13 @@ const App = () => {
                             <Route exact path='/news'><News /></Route>
                         </Switch>
                     </Layout>
+
+                    <div className='footer'>
+                        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center'}}>
+                            Cryptoverse <br/>
+                            All rights reserved
+                        </Typography.Title>
+                    </div>
                 </div>
             </div>
         </Router>
