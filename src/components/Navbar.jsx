b1d073ee72c4111859a485b8821a5fc1465b11c1
Deepayan <deepayanmukherjee1312@gmail.com>
2021-09-28 19:57:38 +0530

    Added responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,28 @@ import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 import icon from '../images/cryptocurrency.png'
 const Navbar = () => {
+
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [screenSize, setScreenSize] = useState(undefined);
+
+    useEffect(() => {
+        const handleResize = () => setScreenSize(window.innerWidth);
+
+        window.addEventListener('resize', handleResize);
+
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    useEffect(() => {
+        if (screenSize <= 800) {
+        setActiveMenu(false);
+        } else {
+        setActiveMenu(true);
+        }
+    }, [screenSize]);
+
     return (
         <div className='nav-container'>
             <div className='logo-container'>
@@ -11,9 +33,11 @@ const Navbar = () => {
                 <Typography.Title level={2} className='logo'>
                     <Link to='/'>CryptoVerse</Link>
                 </Typography.Title>
+
+                <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
             </div>
 
-            <Menu theme='dark'>
+            {activeMenu && (<Menu theme='dark'>
                 <Menu.Item icon={ <HomeOutlined/> }>
                 <Link to='/'>Home</Link>
                 </Menu.Item>
@@ -30,6 +54,7 @@ const Navbar = () => {
                 <Link to='/news'>News</Link>
                 </Menu.Item>
             </Menu>
+            )}
         </div>
     )
 }
